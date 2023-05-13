12509d6b3bf1165a9c7857762b01ad72fe2a95d6
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2023-05-13 20:57:39 +0200

    Cli Config initialization

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,33 @@
+use std::{env, process};
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for \"{}\"", config.query);
+
+    run(config);
+}
+
+struct Config {
+    query: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+
+        Ok(Config { query })
+    }
+}
+
+fn run(config: Config) {
+    println!("Running with:\n{}", config.query);
 }
