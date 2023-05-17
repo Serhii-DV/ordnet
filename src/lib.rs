4ee96940f416fb5e193afbda6722f7111524391a
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2023-05-17 21:59:19 +0200

    Added `get_article_content` method with test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use scraper::{Html, Selector};
 use std::error::Error;
 
 pub struct Config {
@@ -19,4 +20,39 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("Running with:\n{}", config.query);
     Ok(())
+}
+
+pub fn get_article_content(article_class: &str, contents: &str) -> Vec<String> {
+    let mut results = Vec::new();
+
+    let mut article_selector = "div.".to_owned();
+    article_selector.push_str(article_class);
+
+    let fragment = Html::parse_fragment(contents);
+    let article_selector = Selector::parse(&article_selector).unwrap();
+
+    let article_div = fragment.select(&article_selector).next().unwrap();
+    let article_inner = article_div.inner_html();
+
+    results.push(article_inner);
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_get_article_content() {
+        let article_class = "artikel";
+        let contents = "\
+<div>
+    <div class=\"artikel\">Article content</div>
+</div>";
+
+        assert_eq!(
+            vec!["Article content"],
+            get_article_content(article_class, contents)
+        );
+    }
 }
